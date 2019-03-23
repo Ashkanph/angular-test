72a83dd315a708ef15f0b2d90be1e53d14fdba28
Ashkan Paleganeh <ashkanpaleganeh@gmail.com>
2019-03-23 19:46:32 +0430

    Frontend: Add logout button to the header

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
   private currentUser:any = null;
-  constructor() { }
+  constructor(private http:HttpClient,
+              private router: Router) { }
   
   setCurrentUser(user:any){
     this.currentUser = user;
@@ -12,5 +15,16 @@ export class AuthService {
 
   getCurrentUser(){
     return this.currentUser;
+  }
+
+  logout(){
+    this.http.get("http://localhost:3000/api/v0.0.1/logout").
+      subscribe((res:any) => { 
+        if(res.status === 0){
+          this.router.navigate(['/login']);
+          return true;
+        }else
+          return false;
+      });  
   }
 }
